486f26e1e1f783125de7a127d5218787b11a474b
eskander-gtari <eskander.gtari.h@gmail.com>
2019-12-12 15:50:49 +0100

    Second commit

---
+++ src/main/java/com/scheidtbachmann/internal/tripcost/TripcostApplication.java
@@ -0,0 +1,13 @@
+package com.scheidtbachmann.internal.tripcost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripcostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripcostApplication.class, args);
+	}
+
+}
